4f2ecbb427ca414347275e6acfbf5c7c45e30088
ksjogo <jogo@kronberger-spiele.de>
2016-10-19 12:18:54 +0200

    Download emacs-module.h for building

--- build.rs
+++ build.rs
@@ -13,6 +13,7 @@
 // the License.
 
 extern crate bindgen;
+extern crate hyper;
 
 use std::env;
 use std::fs::File;
@@ -20,15 +21,31 @@ use std::io;
 use std::io::prelude::*;
 use std::path::Path;
 
-fn generate_emacs_bindings(header: &str, out_dir: &str) -> io::Result<()> {
-    let dest_path = Path::new(out_dir).join("emacs.rs");
+use hyper::Client;
+use hyper::header::Connection;
 
-    let mut bindings = bindgen::Builder::new(header);
+fn generate_emacs_bindings(out_dir: &str) -> io::Result<()> {
+    let origin_path = Path::new(&out_dir).join("emacs-module.h");
+    //download header file first
+    let client = Client::new();
+    let mut response = client.get("https://github.com/emacs-mirror/emacs/raw/master/src/emacs-module.h").
+        header(Connection::close()).send().unwrap();
+    let mut content = String::new();
+    response.read_to_string(&mut content).unwrap();
+    //fix intmax_t for the moment
+    content = content.replace("intmax_t","int");
+    //write locally for bindgen
+    let mut f = File::create(&origin_path).expect("Unable to create emacs-module.h file");
+    f.write_all(content.as_bytes()).unwrap();
+
+    //generate bindings from downloaded header
+    let mut bindings = bindgen::Builder::new(origin_path.to_string_lossy().into_owned());
     let generated_bindings = bindings.builtins()
         .forbid_unknown_types()
         .generate()
         .expect("Failed to generate bindings");
-
+    //and write them into the emacs.rs
+    let dest_path = Path::new(out_dir).join("emacs.rs");
     let mut file = try!(File::create(dest_path));
     try!(file.write(b"pub mod emacs {\n"));
     try!(file.write(generated_bindings.to_string().as_bytes()));
@@ -38,6 +55,5 @@ fn generate_emacs_bindings(header: &str, out_dir: &str) -> io::Result<()> {
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
-
-    generate_emacs_bindings("emacs-module.h", &out_dir).unwrap();
+    generate_emacs_bindings(&out_dir).unwrap();
 }
